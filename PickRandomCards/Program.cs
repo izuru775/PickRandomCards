64de4d7c95e56089a93a4960af7961dc6a493d4c
izuru775 <izuru775@gmail.com>
2021-10-23 19:24:59 +1100

    Get user input and pick specific number of cards

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -6,7 +6,21 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line,out int numberOfCards))
+            {
+                string[] pickedCards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (var item in pickedCards)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number you entered is invalid!");
+            }
         }
     }
 }
